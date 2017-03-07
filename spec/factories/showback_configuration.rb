08e5de9e7e109a8483ee461d78ae2c83f6c0b874
Esteban Dorado <fadoro@gmail.com>
2017-03-07 17:05:22 +0100

    Reviewed PR with changes in model, specs and migration

--- spec/factories/showback_configuration.rb
+++ spec/factories/showback_configuration.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :showback_configuration do
 
-    sequence(:name)          { |s| "name #{s}" }
+  sequence(:name)          { |s| "name #{s}" }
     sequence(:description)   { |s| "Description #{s}" }
     measure                  'Integer'
     types                    ["cpu_usage_rate_average", "cpu_usagemhz_rate_average"]
--- spec/factories/showback_event.rb
+++ spec/factories/showback_event.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
-  factory :showback do
-    data                      { { 3.hour.ago => {"cpu_usage_rate_average" => 2} } }
+  factory :showback_event do
     id_obj                    1000001
     type_obj                  'VM'
     event_init                3.hour.ago

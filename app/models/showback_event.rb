69be9884d40b8c73e120d6a90a36677cb2343af0
Esteban Dorado <fadoro@gmail.com>
2017-03-08 08:55:42 +0100

    fixed bad attributes for model

--- app/models/showback_event.rb
+++ app/models/showback_event.rb
@@ -2,8 +2,8 @@ class ShowbackEvent < ApplicationRecord
   belongs_to :showback_configuration
 
   validates :data,       :presence => true
-  validates :event_init, :presence => true
-  validates :event_end,  :presence => true
+  validates :start_time, :presence => true
+  validates :end_time,   :presence => true
   validates :id_obj,     :presence => true
   validates :type_obj,   :presence => true
 

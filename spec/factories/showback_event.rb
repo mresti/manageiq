e505388376bbbf6c9c3e5b0ddd6d5e65a385ad1f
Esteban Dorado <fadoro@gmail.com>
2017-03-08 08:34:03 +0100

    Updating model, specs

--- spec/factories/showback_event.rb
+++ spec/factories/showback_event.rb
@@ -1,9 +1,12 @@
+DATA = {3.hour.ago => {"cpu_usage_rate_average" => 2}}
+
 FactoryGirl.define do
   factory :showback_event do
-    id_obj                    1000001
+    data                      DATA
+    sequence(:id_obj)         { 1000001 }
     type_obj                  'VM'
-    event_init                3.hour.ago
-    event_end                 1.hour.ago
+    start_time                3.hour.ago
+    end_time                  1.hour.ago
     showback_configuration
   end
 end
